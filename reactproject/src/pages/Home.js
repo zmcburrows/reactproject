c1fe4df6223ac2190cc7a23cb3304230c0ba47f5
zmcburrows <90242589+zmcburrows@users.noreply.github.com>
2023-01-03 12:00:01 -0500

    updated link on Home

--- reactproject/src/pages/Home.js
+++ reactproject/src/pages/Home.js
@@ -6,8 +6,8 @@ import React from 'react';
 function App() {
   return (
     <div className='homeApp'> 
-  <div className='homeInstructions'>Choose a Boredom Buster and go down the rabbit hole to your heart's content:</div>
-  <a className = 'availableApp' href='./randompage'><img
+  <div className='homeInstructions'>Choose a Boredom Buster and go down the rabbit hole to your hearts content:</div>
+  <a className = 'availableApp' href='./RandomPage.js'><img
           src="https://upload.wikimedia.org/wikipedia/commons/d/de/Wikipedia-logo_%28inverse%29.png"
           alt="Wikipedia Logo"
           width="300"
