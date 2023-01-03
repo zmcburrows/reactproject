a1d9f39873ca58bcac13967ceff5a868b89a72a2
zmcburrows <90242589+zmcburrows@users.noreply.github.com>
2023-01-03 12:12:10 -0500

    updated home page link

--- reactproject/src/pages/Home.js
+++ reactproject/src/pages/Home.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className='homeApp'> 
   <div className='homeInstructions'>Choose a Boredom Buster and go down the rabbit hole to your hearts content:</div>
-  <a className = 'availableApp' href='./RandomPage.js'><img
+  <a className = 'availableApp' href= "randompage"><img
           src="https://upload.wikimedia.org/wikipedia/commons/d/de/Wikipedia-logo_%28inverse%29.png"
           alt="Wikipedia Logo"
           width="300"
